ccb2270be22421ab78516fa28ecbb2abcf8fcb47
unknown <lesleyjosh>
2024-08-22 16:00:39 +0800

    desktop need

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { transform } = require("sucrase");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/*.{html,js}"],
@@ -10,13 +12,19 @@ module.exports = {
         primary: "#1e3932",
         secondary: "#00754a",
         tertiary: "#d4e9e2",
+        quaternary: "#f2f0eb",
       },
       backgroundImage: {
         bgRewards: "url('../img/mobileReward.webp')",
-        bgRewardsDT: "url('../img/bg-rewardpc.webp')",
-      }
+        bgRewardsDT: "url('../img/bg-rewardpc.png')",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
     },
   },
   plugins: [],
 };
-
